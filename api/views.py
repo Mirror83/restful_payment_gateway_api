d7fb8a8220401588cc325adbf7d27a04ffd13c81
Mirror83 <glenochieng045@gmail.com>
2025-06-06 17:05:40 +0300

    refactor: reformat code to reduce indent nesting

--- api/views.py
+++ api/views.py
@@ -20,42 +20,51 @@ def get_paystack_client():
 
 @csrf_exempt
 def initialize_payment(request: HttpRequest):
-    if request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = PaymentInfo(data=data)
-        if serializer.is_valid():
-            with get_paystack_client() as client:
-                request_data = {
-                    "email": data["customer_email"],
-                    # Multiply by 100 to convert into Paystack subunits
-                    "amount": int(data["amount"] * 100)
-                }
-                response = client.post("/transaction/initialize", json=request_data)
-                if not response.is_success:
-                    return JsonResponse(response.json(), status=status.HTTP_400_BAD_REQUEST)
+    if request.method != "POST":
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-                data = response.json()
-                response_serializer = PaystackTransactionInitResponse(data=data)
-                if not response_serializer.is_valid():
-                    return JsonResponse(response_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # Validate request body data
+    data = JSONParser().parse(request)
+    serializer = PaymentInfo(data=data)
+    if not serializer.is_valid():
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-                return JsonResponse(response_serializer.data, status=status.HTTP_200_OK)
-        else:
-            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    with get_paystack_client() as client:
+        request_data = {
+            "email": data["customer_email"],
+            # Multiply by 100 to convert into Paystack subunits
+            "amount": int(data["amount"] * 100)
+        }
+        response = client.post("/transaction/initialize", json=request_data)
 
-    return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        if not response.is_success:
+            return JsonResponse(response.json(), status=status.HTTP_400_BAD_REQUEST)
+        data = response.json()
+
+        # Validate Paystack API call response data
+        paystack_response_serializer = PaystackTransactionInitResponse(data=data)
+        if not paystack_response_serializer.is_valid():
+            return JsonResponse(paystack_response_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Everything went well, build response
+        return JsonResponse(paystack_response_serializer.data, status=status.HTTP_200_OK)
+
 
 def get_payment_status(request: HttpRequest, payment_id: str):
-    if request.method == "GET":
-        with get_paystack_client() as client:
-            response = client.get(f"/transaction/verify/{payment_id}")
-            if not response.is_success:
-                return JsonResponse({"payment_id": payment_id,"status": "failed"}, status=status.HTTP_400_BAD_REQUEST)
-            data = response.json()
-            serializer = PaystackTransactionStatusResponse(data=data)
-            if not serializer.is_valid():
-                return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method != "GET":
+        return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    with get_paystack_client() as client:
+        # Call Paystack API to retrieve payment status
+        response = client.get(f"/transaction/verify/{payment_id}")
+        if not response.is_success:
+            return JsonResponse(
+                {"payment_id": payment_id,"status": "failed"},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        data = response.json()
+        serializer = PaystackTransactionStatusResponse(data=data)
+        if not serializer.is_valid():
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         return JsonResponse(serializer.data, status=status.HTTP_200_OK)
-
-    return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
