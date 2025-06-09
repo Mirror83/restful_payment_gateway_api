843f9753eab8428b18b9c5cd297244975658ad51
Mirror83 <glenochieng045@gmail.com>
2025-06-09 19:39:55 +0300

    fix: include trailing url to get_payment_status route

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ app_name = "api"
 
 urlpatterns = [
     path("v1/payments/", views.initialize_payment, name="initialize_payment"),
-    path("v1/payments/<str:payment_id>", views.get_payment_status, name="get_payment_status"),
+    path("v1/payments/<str:payment_id>/", views.get_payment_status, name="get_payment_status"),
 ]
