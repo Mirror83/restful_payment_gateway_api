f3be19d46705a64819633d34774c7886b8caf55d
Mirror83 <glenochieng045@gmail.com>
2025-06-12 08:27:20 +0300

    feat: create a Payment model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    PAYMENT_STATUS = (
+        ("pending", "Pending"),
+        ("success", "Success"),
+        ("failed", "Failed"),
+    )
+
+    reference = models.CharField(max_length=100, unique=True)
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(choices=PAYMENT_STATUS, default=PAYMENT_STATUS[0][0], max_length=10)
+    paid_at = models.DateTimeField(null=True, blank=True)
+    initiated_at = models.DateTimeField(auto_now_add=True)
+
